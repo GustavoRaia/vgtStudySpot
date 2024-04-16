ff32712f3ec691d2c4477f2bf1035b8dab40f3e7
Gustavo Raia <gustavo.raia02@gmail.com>
2024-04-16 18:51:35 -0300

    Estilização de Tables

--- vgt-study-spot/src/components/BDD/Sql/Sql.jsx
+++ vgt-study-spot/src/components/BDD/Sql/Sql.jsx
@@ -80,7 +80,7 @@ const Sql = () => (
 
                 <h5>Atributos Simples</h5>
                 <p>São os atributos mais comuns existentes. São indivisíveis e representam um valor único, como <i>CPF</i>, <i>Nome</i>,
-                <i>Preço</i>, <i>Data de Fabricação</i>.</p>
+                    <i>Preço</i>, <i>Data de Fabricação</i>.</p>
 
                 <h5>Atributos Compostos</h5>
                 <p>Atributos que dependem de um conjunto de outros "sub atributos", ou que podem ser divididos em atributos menores. Como ocorre com <i>endereço</i> que é composto por <i>CEP</i>, <i>Rua</i>, <i>Número</i>, <i>Complemento</i></p>
@@ -141,81 +141,49 @@ const Sql = () => (
 
         </details>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <details className="materia">
             <summary>SQL</summary>
             <p>Structured Query Language - Linguagem criada para manipular dados em bancos de dados relacionais.</p>
+            <p>
+                <ul>
+                    <li>MySQL</li>
+                    <li>PostgreSQL</li>
+                    <li>SQL Server</li>
+                    <li>Oracle Database </li>
+                    <li>SQLite</li>
+                </ul>
+            </p>
 
             <details>
                 <summary>Sublinguagens</summary>
-                <details>
-                    <summary>DQL</summary>
-                    <p>
-                        <h4>Data Query Language</h4>
-                    </p>
-                    <p>Responsável pelas leituras e consultas de dados no banco. As consultas podem incluir filtros,
-                        ordenação, agrupamento e junção de dados de várias tabelas.</p>
-                    <details>
-                        <summary>Comandos</summary>
-                        <br />
-                        <div className="div-comandos">
-                            <span>SELECT</span>
-                            <span>FROM</span>
-                            <span>WHERE</span>
-                            <span>AND</span>
-                            <span>OR</span>
-                            <span>INNER JOIN</span>
-                            <span>LEFT JOIN</span>
-                            <span>RIGHT JOIN</span>
-                            <span>CROSS JOIN</span>
-                            <span>GROUP BY</span>
-                            <span>ORDER BY</span>
-                            <span>LIMIT</span>
-                            <span>OFFSET</span>
-                            <span>HAVING</span>
-                        </div>
-                        <ul>
-                            <li>SELECT - Seleciona uma coluna de uma tabela</li>
-                            <li>FROM</li>
-                            <li>WHERE</li>
-                            <li>LIMIT</li>
-                            <li>SELECT</li>
-                            <li>SELECT</li>
-                            <li>SELECT</li>
-                        </ul>
-                    </details>
-                    {/*  */}
-                    <details>
-                        <summary>Exemplos</summary>
-                        <br />
-                        <div className="div-exemplos">
-                            <p className="comentario">-- SELECT básico</p>
-                            <p className="comentario">-- O "*" seleciona e retorna todas as colunas da tabela</p>
-                            <p>SELECT * FROM compradores;</p>
-                        </div>
-                        <div className="div-exemplos">
-                            <p className="comentario">-- SELECT com DISTINCT</p>
-                            <p className="comentario">-- O DISTINCT seleciona a primeira ocorrência de um valor na tabela, sem repetição</p>
-                            <p>SELECT DISTINCT cidade, estado FROM compradores;</p>
-                        </div>
-                        <div className="div-exemplos">
-                            <p className="comentario">-- SELECT com condição de seleção (WHERE)</p>
-                            <p className="comentario">-- Utilização de Subquery</p>
-                            <p>SELECT * FROM  <br />
-                                WHERE codigo_produto NOT IN (SELECT codigo_produto FROM itens_de_venda);</p>
-                        </div>
-                        <div className="div-exemplos">
-                            <p className="comentario">-- SELECT utilizando CASE</p>
-                            <p>SELECT cidade, estado, <br />
-                                &nbsp;&nbsp;CASE <br />
-                                &nbsp;&nbsp;&nbsp;&nbsp;WHEN estado LIKE 'RJ' THEN 'Rio de Janeiro' <br />
-                                &nbsp;&nbsp;&nbsp;&nbsp;WHEN estado LIKE 'PA' THEN 'Pará' <br />
-                                &nbsp;&nbsp;&nbsp;&nbsp;WHEN estado LIKE 'CE' THEN 'Ceará' <br />
-                                &nbsp;&nbsp;&nbsp;&nbsp;ELSE 'Outro' <br />
-                                &nbsp;&nbsp;END AS estado_texto <br />
-                                FROM fornecedores;</p>
-                        </div>
-                    </details>
-                </details>
+
                 <details>
                     <summary>DDL</summary>
                     <p>
@@ -243,15 +211,64 @@ const Sql = () => (
                         <div className="div-exemplos">
                             <p className="comentario">-- Adicionando um comentário</p>
                             <p>CREATE TABLE cliente ( <br />
+                                &nbsp;&nbsp;id INT UNIQUE NOT NULL, <br />
                                 &nbsp;&nbsp;telefone VARCHAR(255) NOT NULL, <br />
                                 &nbsp;&nbsp;nome VARCHAR(255) NOT NULL, <br />
                                 &nbsp;&nbsp;endereco VARCHAR(255) NULL, <br />
                                 &nbsp;&nbsp;ponto_referencia VARCHAR(255) NULL, <br />
                                 &nbsp;&nbsp;nascimento DATE NULL, <br />
-                                &nbsp;&nbsp;CONSTRAINT pk_cliente PRIMARY KEY (telefone) <br />
+                                &nbsp;&nbsp;CONSTRAINT pk_cliente PRIMARY KEY (id) <br />
                                 );
                             </p>
                         </div>
+
+                        <table>
+                            <tr>
+                                <th colSpan={6}>cliente</th>
+                            </tr>
+                            <tr>
+                                <td>id</td>
+                                <td>telefone</td>
+                                <td>nome</td>
+                                <td>endereco</td>
+                                <td>ponto_referencia</td>
+                                <td>nascimento</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>McDonald's</td>
+                                <td>Tocha 1</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>null</td>
+                                <td>Tocha 1</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>Casa do Puterrier</td>
+                                <td>Tocha 1</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>Tocha 1</td>
+                                <td>null</td>
+                                <td>Tocha 1</td>
+                            </tr>
+
+                        </table>
+
                         <div className="div-exemplos">
                             <p classNameName='comentario'>-- Criação de Tabela</p>
                             <p classNameName='comentario'>-- Utilização de DEFAULT (valor pré definido caso não preenchido pelo usuário)</p>
@@ -272,6 +289,7 @@ const Sql = () => (
                         </div>
                     </details>
                 </details>
+
                 <details>
                     <summary>DML</summary>
                     <p>
@@ -363,6 +381,156 @@ const Sql = () => (
                         </details>
                     </details>
                 </details>
+
+                <details>
+                    <summary>DQL</summary>
+                    <p>
+                        <h4>Data Query Language</h4>
+                    </p>
+                    <p>Responsável pelas leituras e consultas de dados no banco. As consultas podem incluir filtros, ordenação, agrupamento e junção de dados de várias tabelas.</p>
+                    <details>
+                        <summary>Comandos</summary>
+                        <br />
+                        <div className="div-comandos">
+                            <span>SELECT</span>
+                            <span>FROM</span>
+                            <span>WHERE</span>
+                            <span>AND</span>
+                            <span>OR</span>
+                            <span>INNER JOIN</span>
+                            <span>LEFT JOIN</span>
+                            <span>RIGHT JOIN</span>
+                            <span>CROSS JOIN</span>
+                            <span>GROUP BY</span>
+                            <span>ORDER BY</span>
+                            <span>LIMIT</span>
+                            <span>OFFSET</span>
+                            <span>HAVING</span>
+                        </div>
+                        <ul>
+                            <li>SELECT - Seleciona uma coluna de uma tabela</li>
+                            <li>FROM - Especifica o nome da tabela</li>
+                            <li>WHERE - Condição a ser cumprida</li>
+                            <li>LIMIT - Limita a quantidade de linhas a serem retornadas</li>
+                            <li>OFFSET - Pula uma quantidade definida de linhas retornadas</li>
+                            <li>SELECT</li>
+                            <li>SELECT</li>
+                        </ul>
+                    </details>
+
+                    <details>
+                        <summary>Exemplos</summary>
+                        <br />
+                        <div className='obs'>
+                            <p>Para os exemplos, será utilizada a base de dados <b>sakila</b>, que já vem instalada no MySQL Workbench.</p>
+                            <p>Em alguns casos, será utilizada a cláusula <span>LIMIT</span>, já que algumas consultas retornam uma grande quantidade de linhas.</p>
+                        </div>
+                        <br />
+                        <div className="div-exemplos">
+                            <p className="comentario">-- SELECT básico</p>
+                            <p className="comentario">-- O "*" seleciona e retorna todas as colunas da tabela</p>
+                            <p>SELECT * FROM language;</p>
+                        </div>
+                        <p><b>Tabela Retornada:</b></p>
+                        <p>
+                            <table>
+                                <tr>
+                                    <th colSpan={3}>language</th>
+                                </tr>
+                                <tr>
+                                    <td>language_id</td>
+                                    <td>name</td>
+                                    <td>last_update</td>
+                                </tr>
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>English</td>
+                                    <td>2006-02-15 05:02:19</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Italian</td>
+                                    <td>2006-02-15 05:02:19</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Japanese</td>
+                                    <td>2006-02-15 05:02:19</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>Mandarin</td>
+                                    <td>2006-02-15 05:02:19</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>French</td>
+                                    <td>2006-02-15 05:02:19</td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>German</td>
+                                    <td>2006-02-15 05:02:19</td>
+                                </tr>
+
+                            </table>
+                        </p>
+
+                        <div className="div-exemplos">
+                            <p className="comentario">-- SELECT com DISTINCT</p>
+                            <p className="comentario">-- O DISTINCT seleciona a primeira ocorrência de um valor na tabela, sem repetição</p>
+                            <p className='comentario'>-- Gostariamos de listar todos os valores possíveis de aluguel de um filme</p>
+                            <p>SELECT DISTINCT rental_rate FROM film;</p>
+                        </div>
+                        <p><b>Tabela Retornada:</b></p>
+                        <p>
+                            <table>
+                                <tr>
+                                    <th colSpan={1}>film</th>
+                                </tr>
+                                <tr>
+                                    <td>rental_rate</td>
+                                </tr>
+
+                                <tr>
+                                    <td>0.99</td>
+                                </tr>
+                                <tr>
+                                    <td>4.99</td>
+                                </tr>
+                                <tr>
+                                    <td>2.99</td>
+                                </tr>
+
+                            </table>
+                        </p>
+
+                        <div className='obs'>
+                            Perceba que só existem 3 valores possíveis para o valor de aluguel de um filme.
+                        </div>
+
+                        <div className="div-exemplos">
+                            <p className="comentario">-- SELECT com condição de seleção (WHERE)</p>
+                            <p className="comentario">-- Utilização de Subquery</p>
+                            <p>SELECT * FROM  <br />
+                                WHERE codigo_produto NOT IN (SELECT codigo_produto FROM itens_de_venda);</p>
+                        </div>
+
+                        <div className="div-exemplos">
+                            <p className="comentario">-- SELECT utilizando CASE</p>
+                            <p>SELECT cidade, estado, <br />
+                                &nbsp;&nbsp;CASE <br />
+                                &nbsp;&nbsp;&nbsp;&nbsp;WHEN estado LIKE 'RJ' THEN 'Rio de Janeiro' <br />
+                                &nbsp;&nbsp;&nbsp;&nbsp;WHEN estado LIKE 'PA' THEN 'Pará' <br />
+                                &nbsp;&nbsp;&nbsp;&nbsp;WHEN estado LIKE 'CE' THEN 'Ceará' <br />
+                                &nbsp;&nbsp;&nbsp;&nbsp;ELSE 'Outro' <br />
+                                &nbsp;&nbsp;END AS estado_texto <br />
+                                FROM fornecedores;</p>
+                        </div>
+                    </details>
+                </details>
+
                 <details>
                     <summary>DCL</summary>
                     <p>
@@ -439,7 +607,7 @@ const Sql = () => (
                     <summary>Cursor</summary>
                     <br />
                     <div className="div-exemplos">
-                        <p classNameName='comentario'>-- Ruyter Poubel</p>
+                        <p classNameName='comentario'>-- Comentário Inútil</p>
                     </div>
 
                 </details>
