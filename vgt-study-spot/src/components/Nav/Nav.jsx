f681bd15ad33ca00efb75510170e0750eb22734a
gvfirmeza <gvfirmeza@gmail.com>
2024-04-19 11:46:11 -0300

    fix: solving single page application problem

--- vgt-study-spot/src/components/Nav/Nav.jsx
+++ vgt-study-spot/src/components/Nav/Nav.jsx
@@ -1,11 +1,12 @@
 import "./Nav.css"
 import Logo from "../../logo.svg"
+import { Link } from "react-router-dom";
 
 const Nav = () => (
         <div className="barra-nav">
             <ul className="barra-nav_lista">
-                <li className="barra-nav_item"><a className="barra-nav_A" href="./">StudySpot</a></li>
-                <li className="barra-nav_item"><a className="barra-nav_A" href="./quiz">Quiz</a></li>
+                <li className="barra-nav_item"><Link className="barra-nav_A" to="/">StudySpot</Link></li>
+                <li className="barra-nav_item"><Link className="barra-nav_A" to="/quiz">Quiz</Link></li>
             </ul>
         </div>
 );
