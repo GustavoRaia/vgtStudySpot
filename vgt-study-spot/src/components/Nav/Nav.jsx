f24cb16391438649f8490c84ac6801bee730457e
gvfirmeza <gvfirmeza@gmail.com>
2024-07-17 02:08:29 -0300

    feat: something big is coming...

--- vgt-study-spot/src/components/Nav/Nav.jsx
+++ vgt-study-spot/src/components/Nav/Nav.jsx
@@ -1,13 +1,17 @@
-import "./Nav.css"
+import "./Nav.css";
 import { Link } from "react-router-dom";
+import Logo from "./logo.png";
 
 const Nav = () => (
-        <div className="barra-nav">
-            <ul className="barra-nav_lista">
-                <li className="barra-nav_item"><Link className="barra-nav_A" to="/">StudySpot</Link></li>
-                <li className="barra-nav_item"><Link className="barra-nav_A" to="/quiz">Quiz</Link></li>
-            </ul>
+    <div className="barra-nav">
+        <div className="logo">
+            <Link className="barra-nav_A" to="/"><img className="logo_img" src={Logo} alt="Logo" /></Link>
         </div>
+        <ul className="barra-nav_lista">
+            <li className="barra-nav_item"><Link className="barra-nav_A" to="/">Almanaque</Link></li>
+            <li className="barra-nav_item"><Link className="barra-nav_A" to="/quiz">Quiz</Link></li>
+        </ul>
+    </div>
 );
 
 export default Nav;
